5fa8bb6ca8448de51dd300fc69f41666c0b6c394
Wilmer Arambula <terabytesoftw@gmail.com>
2020-10-30 11:22:57 -0300

    Enhacements.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,10 +6,18 @@ namespace Yiisoft\Yii\Bootstrap5\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use RuntimeException;
 use Yiisoft\Aliases\Aliases;
-use Yiisoft\Files\FileHelper;
+use Yiisoft\Assets\AssetConverterInterface;
+use Yiisoft\Assets\AssetConverter;
+use Yiisoft\Assets\AssetManager;
+use Yiisoft\Assets\AssetPublisher;
+use Yiisoft\Assets\AssetPublisherInterface;
 use Yiisoft\Di\Container;
+use Yiisoft\Factory\Definitions\Reference;
+use Yiisoft\Files\FileHelper;
 use Yiisoft\Widget\WidgetFactory;
 
 use function closedir;
@@ -20,35 +28,26 @@ use function str_replace;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * @var Aliases $aliases
-     */
-    protected $aliases;
-
-    /**
-     * @var ContainerInterface $container
-     */
-    private $container;
+    protected Aliases $aliases;
+    protected AssetManager $assetManager;
+    private ContainerInterface $container;
 
     protected function setUp(): void
     {
-        $this->container = new Container([]);
-        $this->aliases = new Aliases([
-            '@root' => dirname(__DIR__, 1),
-            '@public' => '@root/tests/public',
-            '@basePath' => '@public/assets',
-            '@baseUrl'  => '/',
-            '@npm' => '@root/node_modules',
-            '@view' => '@public/view',
-        ]);
+        $this->container = new Container($this->config());
+
+        $this->aliases = $this->container->get(Aliases::class);
+        $this->assetManager = $this->container->get(AssetManager::class);
 
         WidgetFactory::initialize($this->container, []);
     }
 
     protected function tearDown(): void
     {
-        $this->container = null;
-        $this->removeAssets('@basePath');
+        $this->removeAssets('@assets');
+
+        unset($this->aliases, $this->assetManager, $this->container);
+
         parent::tearDown();
     }
 
@@ -105,5 +104,36 @@ abstract class TestCase extends BaseTestCase
         }
 
         closedir($handle);
+    }
+
+    private function config(): array
+    {
+        return [
+            Aliases::class => [
+                '__class' => Aliases::class,
+                '__construct()' => [
+                    [
+                        '@root' => dirname(__DIR__, 1),
+                        '@public' => '@root/tests/public',
+                        '@assets' => '@public/assets',
+                        '@assetsUrl'  => '/',
+                        '@npm' => '@root/node_modules',
+                        '@view' => '@public/view'
+                    ]
+                ]
+            ],
+
+            LoggerInterface::class => NullLogger::class,
+
+            AssetConverterInterface::class => AssetConverter::class,
+
+            AssetPublisherInterface::class => AssetPublisher::class,
+
+            AssetManager::class => [
+                '__class' => AssetManager::class,
+                'setConverter()' => [Reference::to(AssetConverterInterface::class)],
+                'setPublisher()' => [Reference::to(AssetPublisherInterface::class)],
+            ]
+        ];
     }
 }
