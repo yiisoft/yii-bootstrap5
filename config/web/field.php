bdd34dfc3dc1f3f9e3fd573cdb28f4a9b8e8472e
Mr.42 <johan@mr42.me>
2020-12-22 18:50:27 +0100

    Use Composer\InstalledVersions

--- config/web/field.php
+++ config/web/field.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
 use Yiisoft\Form\Widget\Field;
 
-if (class_exists(Field::class) && $params['yiisoft/form']['bootstrap5']['enabled'] === true) {
+if (InstalledVersions::isInstalled('yiisoft/forms') && $params['yiisoft/form']['bootstrap5']['enabled'] === true) {
     return [
         Field::class => fn () => Field::Widget($params['yiisoft/form']['bootstrap5']['fieldConfig']),
     ];
