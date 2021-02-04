583486f66cbb289f062e8bd044b1d4521406093d
Wilmer Arambula <terabytesoftw@gmail.com>
2021-02-04 17:17:11 -0300

    Add methods inmutables, add withEncodeTags.

--- src/Carousel.php
+++ src/Carousel.php
@@ -20,7 +20,7 @@ use function is_string;
  *
  * ```php
  * echo Carousel::widget()
- *     ->items([
+ *     ->withItems([
  *         // the item contains only the image
  *         '<img src="http://twitter.github.io/bootstrap/assets/img/bootstrap-mdo-sfmoma-01.jpg"/>',
  *         // equivalent to the above
@@ -37,16 +37,17 @@ use function is_string;
  */
 final class Carousel extends Widget
 {
-    private ?array $controls = [
+    private array $controls = [
         '<span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span>',
         '<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span>',
     ];
+    private bool $encodeTags = false;
     private bool $showIndicators = true;
     private array $items = [];
     private bool $crossfade = false;
     private array $options = ['data-bs-ride' => 'carousel'];
 
-    protected function run(): string
+    public function run(): string
     {
         if (!isset($this->options['id'])) {
             $this->options['id'] = "{$this->getId()}-carousel";
@@ -61,118 +62,13 @@ final class Carousel extends Widget
 
         $this->registerPlugin('carousel', $this->options);
 
+        if ($this->encodeTags === false) {
+            $this->options = array_merge($this->options, ['encode' => false]);
+        }
+
         return Html::div(
             $this->renderIndicators() . $this->renderItems() . $this->renderControls(),
             $this->options
-        );
-    }
-
-    /**
-     * Renders carousel indicators.
-     */
-    public function renderIndicators(): string
-    {
-        if ($this->showIndicators === false) {
-            return '';
-        }
-
-        $indicators = [];
-
-        for ($i = 0, $count = count($this->items); $i < $count; $i++) {
-            $options = ['data-bs-target' => '#' . $this->options['id'], 'data-bs-slide-to' => $i];
-            if ($i === 0) {
-                /** @psalm-suppress InvalidArgument */
-                Html::addCssClass($options, 'active');
-            }
-            $indicators[] = Html::tag('li', '', $options);
-        }
-
-        return Html::tag('ol', implode("\n", $indicators), ['class' => ['carousel-indicators']]);
-    }
-
-    /**
-     * Renders carousel items as specified on {@see items}.
-     */
-    public function renderItems(): string
-    {
-        $items = [];
-
-        foreach ($this->items as $i => $iValue) {
-            $items[] = $this->renderItem($iValue, $i);
-        }
-
-        return Html::div(implode("\n", $items), ['class' => 'carousel-inner']);
-    }
-
-    /**
-     * Renders a single carousel item
-     *
-     * @param array|string $item a single item from {@see items}
-     * @param int $index the item index as the first item should be set to `active`
-     *
-     * @throws JsonException|RuntimeException if the item is invalid.
-     *
-     * @return string the rendering result.
-     */
-    public function renderItem($item, int $index): string
-    {
-        if (is_string($item)) {
-            $content = $item;
-            $caption = null;
-            $options = [];
-        } elseif (isset($item['content'])) {
-            $content = $item['content'];
-            $caption = ArrayHelper::getValue($item, 'caption');
-
-            if ($caption !== null) {
-                $captionOptions = ArrayHelper::remove($item, 'captionOptions', []);
-                Html::addCssClass($captionOptions, ['widget' => 'carousel-caption']);
-
-                $caption = Html::div($caption, $captionOptions);
-            }
-
-            $options = ArrayHelper::getValue($item, 'options', []);
-        } else {
-            throw new RuntimeException('The "content" option is required.');
-        }
-
-        Html::addCssClass($options, ['widget' => 'carousel-item']);
-
-        if ($index === 0) {
-            Html::addCssClass($options, 'active');
-        }
-
-        return Html::div($content . "\n" . $caption, $options);
-    }
-
-    /**
-     * Renders previous and next control buttons.
-     *
-     * @throws JsonException|RuntimeException if {@see controls} is invalid.
-     *
-     * @return string
-     */
-    public function renderControls(): string
-    {
-        if (isset($this->controls[0], $this->controls[1])) {
-            return Html::a($this->controls[0], '#' . $this->options['id'], [
-                'class' => 'carousel-control-prev',
-                'data-bs-slide' => 'prev',
-                'role' => 'button',
-            ]) . "\n"
-                . Html::a($this->controls[1], '#' . $this->options['id'], [
-                    'class' => 'carousel-control-next',
-                    'data-bs-slide' => 'next',
-                    'role' => 'button',
-                ]);
-        }
-
-        if ($this->controls === null) {
-            return '';
-        }
-
-        throw new RuntimeException(
-            'The "controls" property must be either null or an array of two elements.'
         );
     }
 
@@ -185,11 +81,12 @@ final class Carousel extends Widget
      *
      * @return $this
      */
-    public function controls(?array $value): self
+    public function withControls(array $value): self
     {
-        $this->controls = $value;
+        $new = clone $this;
+        $new->controls = $value;
 
-        return $this;
+        return $new;
     }
 
     /**
@@ -199,11 +96,12 @@ final class Carousel extends Widget
      *
      * @return $this
      */
-    public function crossfade(bool $value): self
+    public function withCrossfade(bool $value): self
     {
-        $this->crossfade = $value;
+        $new = clone $this;
+        $new->crossfade = $value;
 
-        return $this;
+        return $new;
     }
 
     /**
@@ -224,27 +122,29 @@ final class Carousel extends Widget
      *
      * @return $this
      */
-    public function items(array $value): self
+    public function withItems(array $value): self
     {
-        $this->items = $value;
+        $new = clone $this;
+        $new->items = $value;
 
-        return $this;
+        return $new;
     }
 
     /**
      * The HTML attributes for the container tag. The following special options are recognized.
      *
-     * {@see \Yiisoft\Html\Html::renderTagAttributes()} for details on how attributes are being rendered.
+     * {@see Html::renderTagAttributes()} for details on how attributes are being rendered.
      *
      * @param array $value
      *
      * @return $this
      */
-    public function options(array $value): self
+    public function withOptions(array $value): self
     {
-        $this->options = $value;
+        $new = clone $this;
+        $new->options = $value;
 
-        return $this;
+        return $new;
     }
 
     /**
@@ -254,10 +154,164 @@ final class Carousel extends Widget
      *
      * @return $this
      */
-    public function showIndicators(bool $value): self
+    public function withoutShowIndicators(bool $value = false): self
     {
-        $this->showIndicators = $value;
+        $new = clone $this;
+        $new->showIndicators = $value;
 
-        return $this;
+        return $new;
+    }
+
+    /**
+     * Allows you to enable or disable the encoding tags html.
+     *
+     * @param bool $value
+     *
+     * @return self
+     */
+    public function withEncodeTags(bool $value = true): self
+    {
+        $new = clone $this;
+        $new->encodeTags = $value;
+
+        return $new;
+    }
+
+    /**
+     * Renders carousel indicators.
+     */
+    private function renderIndicators(): string
+    {
+        if ($this->showIndicators === false) {
+            return '';
+        }
+
+        $indicators = [];
+
+        for ($i = 0, $count = count($this->items); $i < $count; $i++) {
+            $options = ['data-bs-target' => '#' . $this->options['id'], 'data-bs-slide-to' => $i];
+            if ($i === 0) {
+                /** @psalm-suppress InvalidArgument */
+                Html::addCssClass($options, 'active');
+            }
+            $indicators[] = Html::tag('li', '', $options);
+        }
+
+        $indicatorOptions = ['class' => ['carousel-indicators']];
+
+        if ($this->encodeTags === false) {
+            $indicatorOptions = array_merge($indicatorOptions, ['encode' => false]);
+        }
+
+        return Html::tag('ol', implode("\n", $indicators), $indicatorOptions);
+    }
+
+    /**
+     * Renders carousel items as specified on {@see items}.
+     */
+    private function renderItems(): string
+    {
+        $items = [];
+
+        foreach ($this->items as $i => $iValue) {
+            $items[] = $this->renderItem($iValue, $i);
+        }
+
+        $itemOptions = ['class' => 'carousel-inner'];
+
+        if ($this->encodeTags === false) {
+            $itemOptions = array_merge($itemOptions, ['encode' => false]);
+        }
+
+        return Html::div(implode("\n", $items), $itemOptions);
+    }
+
+    /**
+     * Renders a single carousel item
+     *
+     * @param array|string $item a single item from {@see items}
+     * @param int $index the item index as the first item should be set to `active`
+     *
+     * @throws JsonException|RuntimeException if the item is invalid.
+     *
+     * @return string the rendering result.
+     */
+    private function renderItem($item, int $index): string
+    {
+        if (is_string($item)) {
+            $content = $item;
+            $caption = null;
+            $options = [];
+        } elseif (isset($item['content'])) {
+            $content = $item['content'];
+            $caption = ArrayHelper::getValue($item, 'caption');
+
+            if ($caption !== null) {
+                $captionOptions = ArrayHelper::remove($item, 'captionOptions', []);
+                Html::addCssClass($captionOptions, ['widget' => 'carousel-caption']);
+
+                if ($this->encodeTags === false) {
+                    $captionOptions = array_merge($captionOptions, ['encode' => false]);
+                }
+
+                $caption = Html::div($caption, $captionOptions);
+            }
+
+            $options = ArrayHelper::getValue($item, 'options', []);
+        } else {
+            throw new RuntimeException('The "content" option is required.');
+        }
+
+        Html::addCssClass($options, ['widget' => 'carousel-item']);
+
+        if ($this->encodeTags === false) {
+            $options = array_merge($options, ['encode' => false]);
+        }
+
+        if ($index === 0) {
+            Html::addCssClass($options, 'active');
+        }
+
+        return Html::div($content . "\n" . $caption, $options);
+    }
+
+    /**
+     * Renders previous and next control buttons.
+     *
+     * @throws JsonException|RuntimeException if {@see controls} is invalid.
+     *
+     * @return string
+     */
+    private function renderControls(): string
+    {
+        $controlsOptions0 = [
+            'class' => 'carousel-control-prev',
+            'data-bs-slide' => 'prev',
+            'role' => 'button',
+        ];
+
+        $controlsOptions1 = [
+            'class' => 'carousel-control-next',
+            'data-bs-slide' => 'next',
+            'role' => 'button',
+        ];
+
+        if ($this->encodeTags === false) {
+            $controlsOptions0 = array_merge($controlsOptions0, ['encode' => false]);
+            $controlsOptions1 = array_merge($controlsOptions1, ['encode' => false]);
+        }
+
+        if (isset($this->controls[0], $this->controls[1])) {
+            return Html::a($this->controls[0], '#' . $this->options['id'], $controlsOptions0) . "\n" .
+                Html::a($this->controls[1], '#' . $this->options['id'], $controlsOptions1);
+        }
+
+        if ($this->controls === []) {
+            return '';
+        }
+
+        throw new RuntimeException(
+            'The "controls" property must be either null or an array of two elements.'
+        );
     }
 }
--- tests/CarouselTest.php
+++ tests/CarouselTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Bootstrap5\Tests;
 
+use RuntimeException;
 use Yiisoft\Yii\Bootstrap5\Carousel;
 
 /**
@@ -13,12 +14,12 @@ use Yiisoft\Yii\Bootstrap5\Carousel;
  */
 final class CarouselTest extends TestCase
 {
-    public function testContainerOptions(): void
+    public function testRender(): void
     {
         Carousel::counter(0);
 
         $html = Carousel::widget()
-            ->items([
+            ->withItems([
                 [
                     'content' => '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
                     'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
@@ -63,8 +64,8 @@ HTML;
         Carousel::counter(0);
 
         $html = Carousel::widget()
-            ->crossfade(true)
-            ->items([
+            ->withCrossfade(true)
+            ->withItems([
                 [
                     'content' => '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
                     'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
@@ -90,5 +91,157 @@ HTML;
             ->render();
 
         $this->assertStringContainsString('class="carousel slide carousel-fade"', $html);
+    }
+
+    public function testControlsEmpty(): void
+    {
+        Carousel::counter(0);
+
+        $html = Carousel::widget()
+            ->withControls([])
+            ->withItems([
+                [
+                    'content' => '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
+                    'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
+                    'captionOptions' => [
+                        'class' => ['d-none', 'd-md-block'],
+                    ],
+                ],
+            ])
+            ->render();
+        $expected = <<<HTML
+<div id="w0-carousel" class="carousel slide" data-bs-ride="carousel"><ol class="carousel-indicators"><li class="active" data-bs-target="#w0-carousel" data-bs-slide-to="0"></li></ol><div class="carousel-inner"><div class="carousel-item active"><img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">
+<div class="d-none d-md-block carousel-caption"><h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></div></div></div></div>
+HTML;
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testControlsException(): void
+    {
+        Carousel::counter(0);
+
+        $this->expectException(RuntimeException::class);
+        $html = Carousel::widget()
+            ->withControls(
+                [
+                    '<span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span>',
+                ],
+            )
+            ->withItems([
+                [
+                    'content' => '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
+                    'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
+                    'captionOptions' => [
+                        'class' => ['d-none', 'd-md-block'],
+                    ],
+                ],
+            ])
+            ->render();
+    }
+
+    public function testItemContentEmpty(): void
+    {
+        Carousel::counter(0);
+
+        $this->expectException(RuntimeException::class);
+        $html = Carousel::widget()
+            ->withItems([
+                [
+                    'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
+                    'captionOptions' => [
+                        'class' => ['d-none', 'd-md-block'],
+                    ],
+                ],
+            ])
+            ->render();
+    }
+
+    public function testItemContentString(): void
+    {
+        Carousel::counter(0);
+
+        $html = Carousel::widget()
+            ->withItems([
+                '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
+            ])
+            ->render();
+        $expected = <<<HTML
+<div id="w0-carousel" class="carousel slide" data-bs-ride="carousel"><ol class="carousel-indicators"><li class="active" data-bs-target="#w0-carousel" data-bs-slide-to="0"></li></ol><div class="carousel-inner"><div class="carousel-item active"><img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">
+</div></div><a class="carousel-control-prev" href="#w0-carousel" data-bs-slide="prev" role="button"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a>
+<a class="carousel-control-next" href="#w0-carousel" data-bs-slide="next" role="button"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+HTML;
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testWithOptions(): void
+    {
+        Carousel::counter(0);
+
+        $html = Carousel::widget()
+            ->withItems([
+                [
+                    'content' => '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
+                    'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
+                    'captionOptions' => [
+                        'class' => ['d-none', 'd-md-block'],
+                    ],
+                ],
+            ])
+            ->withOptions(['class' => 'testMe'])
+            ->render();
+        $expected = <<<HTML
+<div id="w0-carousel" class="testMe carousel slide"><ol class="carousel-indicators"><li class="active" data-bs-target="#w0-carousel" data-bs-slide-to="0"></li></ol><div class="carousel-inner"><div class="carousel-item active"><img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">
+<div class="d-none d-md-block carousel-caption"><h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></div></div></div><a class="carousel-control-prev" href="#w0-carousel" data-bs-slide="prev" role="button"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a>
+<a class="carousel-control-next" href="#w0-carousel" data-bs-slide="next" role="button"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+HTML;
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testShowIndicator(): void
+    {
+        Carousel::counter(0);
+
+        $html = Carousel::widget()
+            ->withItems([
+                [
+                    'content' => '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
+                    'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
+                    'captionOptions' => [
+                        'class' => ['d-none', 'd-md-block'],
+                    ],
+                ],
+            ])
+            ->withoutShowIndicators()
+            ->render();
+        $expected = <<<HTML
+<div id="w0-carousel" class="carousel slide" data-bs-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">
+<div class="d-none d-md-block carousel-caption"><h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></div></div></div><a class="carousel-control-prev" href="#w0-carousel" data-bs-slide="prev" role="button"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a>
+<a class="carousel-control-next" href="#w0-carousel" data-bs-slide="next" role="button"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+HTML;
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testEncodeTags(): void
+    {
+        Carousel::counter(0);
+
+        $html = Carousel::widget()
+            ->withEncodeTags()
+            ->withItems([
+                [
+                    'content' => '<img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100">',
+                    'caption' => '<h5>First slide label</h5><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>',
+                    'captionOptions' => [
+                        'class' => ['d-none', 'd-md-block'],
+                    ],
+                ],
+            ])
+            ->render();
+        $expected = <<<HTML
+<div id="w0-carousel" class="carousel slide" data-bs-ride="carousel">&lt;ol class="carousel-indicators"&gt;&amp;lt;li class="active" data-bs-target="#w0-carousel" data-bs-slide-to="0"&amp;gt;&amp;lt;/li&amp;gt;&lt;/ol&gt;&lt;div class="carousel-inner"&gt;&amp;lt;div class="carousel-item active"&amp;gt;&amp;amp;lt;img src="https://via.placeholder.com/800x400?text=First+slide" class="d-block w-100"&amp;amp;gt;
+&amp;amp;lt;div class="d-none d-md-block carousel-caption"&amp;amp;gt;&amp;amp;amp;lt;h5&amp;amp;amp;gt;First slide label&amp;amp;amp;lt;/h5&amp;amp;amp;gt;&amp;amp;amp;lt;p&amp;amp;amp;gt;Nulla vitae elit libero, a pharetra augue mollis interdum.&amp;amp;amp;lt;/p&amp;amp;amp;gt;&amp;amp;lt;/div&amp;amp;gt;&amp;lt;/div&amp;gt;&lt;/div&gt;&lt;a class="carousel-control-prev" href="#w0-carousel" data-bs-slide="prev" role="button"&gt;&amp;lt;span class="carousel-control-prev-icon" aria-hidden="true"&amp;gt;&amp;lt;/span&amp;gt;&amp;lt;span class="visually-hidden"&amp;gt;Previous&amp;lt;/span&amp;gt;&lt;/a&gt;
+&lt;a class="carousel-control-next" href="#w0-carousel" data-bs-slide="next" role="button"&gt;&amp;lt;span class="carousel-control-next-icon" aria-hidden="true"&amp;gt;&amp;lt;/span&amp;gt;&amp;lt;span class="visually-hidden"&amp;gt;Next&amp;lt;/span&amp;gt;&lt;/a&gt;</div>
+HTML;
+        $this->assertEqualsWithoutLE($expected, $html);
     }
 }
