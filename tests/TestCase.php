b4045ca6df3ea301c001893e3d325d767c2583a1
Alexander Makarov <sam@rmcreative.ru>
2021-05-08 23:09:00 +0300

    Remove unnecessary tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,51 +4,30 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Bootstrap5\Tests;
 
-use Yiisoft\Assets\AssetLoader;
-use Yiisoft\Assets\AssetLoaderInterface;
-use function closedir;
-use function is_dir;
-use function opendir;
+
 use PHPUnit\Framework\TestCase as BaseTestCase;
 use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
-use function readdir;
-use RuntimeException;
 use function str_replace;
 use Yiisoft\Aliases\Aliases;
-use Yiisoft\Assets\AssetConverter;
-use Yiisoft\Assets\AssetConverterInterface;
-use Yiisoft\Assets\AssetManager;
-use Yiisoft\Assets\AssetPublisher;
-
-use Yiisoft\Assets\AssetPublisherInterface;
 use Yiisoft\Di\Container;
-use Yiisoft\Factory\Definition\Reference;
-use Yiisoft\Files\FileHelper;
 use Yiisoft\Widget\WidgetFactory;
 
 abstract class TestCase extends BaseTestCase
 {
     protected Aliases $aliases;
-    protected AssetManager $assetManager;
     private ContainerInterface $container;
 
     protected function setUp(): void
     {
-        $this->container = new Container($this->config());
+        parent::setUp();
 
-        $this->aliases = $this->container->get(Aliases::class);
-        $this->assetManager = $this->container->get(AssetManager::class);
-
+        $this->container = new Container();
         WidgetFactory::initialize($this->container, []);
     }
 
     protected function tearDown(): void
     {
-        $this->removeAssets('@assets');
-
-        unset($this->aliases, $this->assetManager, $this->container);
+        unset($this->container);
 
         parent::tearDown();
     }
@@ -80,62 +59,5 @@ abstract class TestCase extends BaseTestCase
         $actual = str_replace(['/', '\\'], '/', $actual);
 
         $this->assertSame($expected, $actual);
-    }
-
-    protected function removeAssets(string $basePath): void
-    {
-        $handle = opendir($dir = $this->aliases->get($basePath));
-        if ($handle === false) {
-            throw new RuntimeException("Unable to open directory: $dir");
-        }
-        while (($file = readdir($handle)) !== false) {
-            if ($file === '.' || $file === '..' || $file === '.gitignore') {
-                continue;
-            }
-
-            $path = $dir . DIRECTORY_SEPARATOR . $file;
-
-            if (is_dir($path)) {
-                FileHelper::removeDirectory($path);
-            } else {
-                FileHelper::unlink($path);
-            }
-        }
-
-        closedir($handle);
-    }
-
-    private function config(): array
-    {
-        return [
-            Aliases::class => [
-                'class' => Aliases::class,
-                '__construct()' => [
-                    [
-                        '@root' => dirname(__DIR__),
-                        '@public' => '@root/tests/public',
-                        '@assets' => '@public/assets',
-                        '@assetsUrl' => '/',
-                        '@vendor' => '@root/vendor',
-                        '@npm' => '@vendor/npm-asset',
-                        '@view' => '@public/view',
-                    ],
-                ],
-            ],
-
-            LoggerInterface::class => NullLogger::class,
-
-            AssetConverterInterface::class => AssetConverter::class,
-
-            AssetPublisherInterface::class => AssetPublisher::class,
-
-            AssetLoaderInterface::class => AssetLoader::class,
-
-            AssetManager::class => [
-                'class' => AssetManager::class,
-                'setConverter()' => [Reference::to(AssetConverterInterface::class)],
-                'setPublisher()' => [Reference::to(AssetPublisherInterface::class)],
-            ],
-        ];
     }
 }
