0dec52c5a79220154df7340d68c524981f25291c
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-08-28 09:51:06 +0300

    Adopt to `yiisoft/definitions`

--- src/ButtonToolbar.php
+++ src/ButtonToolbar.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Bootstrap5;
 
 use Yiisoft\Arrays\ArrayHelper;
-use Yiisoft\Factory\Exception\InvalidConfigException;
+use Yiisoft\Definitions\Exception\InvalidConfigException;
 use Yiisoft\Html\Html;
 
 use function implode;
