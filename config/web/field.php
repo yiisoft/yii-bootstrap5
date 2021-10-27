f0f45168881f24e66bef1a0d35a254eda6db21e5
Wilmer <terabytesoftw@gmail.com>
2021-10-27 13:04:09 -0300

    Clean config yii-bootstrap5.

--- config/web/field.php
+++ config/web/field.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use Yiisoft\Form\Widget\Field;
 
+/** @var array $params */
+
 if ($params['yiisoft/form']['bootstrap5']['enabled'] === true) {
     return [
-        Field::class => static fn () => Field::widget($params['yiisoft/form']['bootstrap5']['fieldConfig']),
+        Field::class => Field::widget($params['yiisoft/form']['bootstrap5']['fieldConfig']),
     ];
 }
