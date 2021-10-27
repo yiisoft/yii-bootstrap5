ec629f90b2e884323420ab1b7b262efea754e5a8
Wilmer <terabytesoftw@gmail.com>
2021-10-27 13:29:55 -0300

    Test other config.

--- config/web/field.php
+++ config/web/field.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Definitions\DynamicReference;
 use Yiisoft\Form\Widget\Field;
 
 /** @var array $params */
 
 if ($params['yiisoft/form']['bootstrap5']['enabled'] === true) {
     return [
-        Field::class => Field::widget($params['yiisoft/form']['bootstrap5']['fieldConfig']),
+        Field::class => DynamicReference::to(
+            static function (array $params): Field {
+                return Field::widget($params['yiisoft/form']['bootstrap5']['fieldConfig']);
+            }
+        ),
     ];
 }
