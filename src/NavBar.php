196a6eb4770cc311cb14977b7e6008d3dd05d8c1
Lucas Bartholemy <luke-@users.noreply.github.com>
2022-05-07 19:18:59 +0200

    Added missing echo in PHPDoc example usage (#86)

--- src/NavBar.php
+++ src/NavBar.php
@@ -48,7 +48,7 @@ use Yiisoft\Html\Html;
  *        ];
  *    }
  *
- *    <?php NavBar::widget()
+ *    <?= NavBar::widget()
  *        ->brandText('My Application Basic')
  *        ->brandUrl('/')
  *        ->options([
