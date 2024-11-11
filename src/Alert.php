afdc356435ca40c4bbe12e58f6712baaa60c9721
Wilmer Arambula <terabytesoftw@gmail.com>
2024-11-11 06:53:33 -0300

    Add missing function import for array_key_exists in Alert.php.

--- src/Alert.php
+++ src/Alert.php
@@ -10,6 +10,7 @@ use Yiisoft\Html\Html;
 use Yiisoft\Html\Tag\Button;
 use Yiisoft\Html\Tag\Div;
 
+use function array_key_exists;
 use function array_merge;
 use function array_filter;
 use function preg_replace;
