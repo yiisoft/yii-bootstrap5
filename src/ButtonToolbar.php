85c328a9dd07232f264e61d2163bf0adadbdb4d5
terabytesoftw <42547589+terabytesoftw@users.noreply.github.com>
2025-02-04 10:05:39 +0000

    Apply Rector changes (CI)

--- src/ButtonToolbar.php
+++ src/ButtonToolbar.php
@@ -52,10 +52,14 @@ use function implode;
 final class ButtonToolbar extends Widget
 {
     private const NAME = 'btn-toolbar';
+
     private array $attributes = [];
+
     /** @psalm-var ButtonGroup[]|Tag[] $buttonGroups */
     private array $buttonGroups = [];
+
     private array $cssClasses = [];
+
     private bool|string $id = true;
 
     /**
