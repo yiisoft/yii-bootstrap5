0485ecbdc94ec50b8464a5ad0d2ea1e5641c98a0
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2025-02-21 09:47:48 -0300

    Clean and improve docs for `Button` class, add `TogglerType` and `ButtonSize` enums, and improve tests. (#225)

---
+++ src/ButtonSize.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Bootstrap5;
+
+/**
+ * Size for the {@see Button} component.
+ */
+enum ButtonSize: string
+{
+    /**
+     * Small size.
+     */
+    case SMALL = 'btn-sm';
+    /**
+     * Large size.
+     */
+    case LARGE = 'btn-lg';
+}
