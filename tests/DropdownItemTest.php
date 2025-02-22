2ad981333e014c08ae55fb37d9d0a9c65d2c0e2b
Wilmer Arambula <terabytesoftw@gmail.com>
2025-02-22 19:34:17 -0300

    Add more tests.

--- tests/DropdownItemTest.php
+++ tests/DropdownItemTest.php
@@ -16,6 +16,45 @@ use Yiisoft\Yii\Bootstrap5\DropdownItem;
 #[Group('dropdown')]
 final class DropdownItemTest extends TestCase
 {
+    public function testButton(): void
+    {
+        $button = DropdownItem::button('content');
+
+        $this->assertSame('button', $button->getType());
+        $this->assertSame('content', $button->getContent());
+        $this->assertFalse($button->isActive());
+        $this->assertFalse($button->isDisabled());
+        $this->assertSame([], $button->getAttributes());
+        $this->assertSame([], $button->getItemAttributes());
+        $this->assertSame('h6', $button->getHeaderTag());
+    }
+
+    public function testDivider(): void
+    {
+        $divider = DropdownItem::divider();
+
+        $this->assertSame('divider', $divider->getType());
+        $this->assertSame('', $divider->getContent());
+        $this->assertFalse($divider->isActive());
+        $this->assertFalse($divider->isDisabled());
+        $this->assertSame([], $divider->getAttributes());
+        $this->assertSame([], $divider->getItemAttributes());
+        $this->assertSame('h6', $divider->getHeaderTag());
+    }
+
+    public function testHeader(): void
+    {
+        $header = DropdownItem::header('content');
+
+        $this->assertSame('header', $header->getType());
+        $this->assertSame('content', $header->getContent());
+        $this->assertFalse($header->isActive());
+        $this->assertFalse($header->isDisabled());
+        $this->assertSame([], $header->getAttributes());
+        $this->assertSame([], $header->getItemAttributes());
+        $this->assertSame('h6', $header->getHeaderTag());
+    }
+
     public function testImmutability(): void
     {
         $dropdownItem = DropdownItem::text('Test');
@@ -28,6 +67,32 @@ final class DropdownItemTest extends TestCase
         $this->assertNotSame($dropdownItem, $dropdownItem->itemAttributes([]));
         $this->assertNotSame($dropdownItem, $dropdownItem->type(DropdownItem::TYPE_BUTTON));
         $this->assertNotSame($dropdownItem, $dropdownItem->url(''));
+    }
+
+    public function testLink(): void
+    {
+        $link = DropdownItem::link('content');
+
+        $this->assertSame('link', $link->getType());
+        $this->assertSame('content', $link->getContent());
+        $this->assertFalse($link->isActive());
+        $this->assertFalse($link->isDisabled());
+        $this->assertSame([], $link->getAttributes());
+        $this->assertSame([], $link->getItemAttributes());
+        $this->assertSame('h6', $link->getHeaderTag());
+    }
+
+    public function testListContent(): void
+    {
+        $listContent = DropdownItem::listContent('content');
+
+        $this->assertSame('custom-content', $listContent->getType());
+        $this->assertSame('content', $listContent->getContent());
+        $this->assertFalse($listContent->isActive());
+        $this->assertFalse($listContent->isDisabled());
+        $this->assertSame([], $listContent->getAttributes());
+        $this->assertSame([], $listContent->getItemAttributes());
+        $this->assertSame('h6', $listContent->getHeaderTag());
     }
 
     public function testText(): void
