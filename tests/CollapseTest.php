6a7c40bef0bfba1264b03e338a59234715ad66fb
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2025-03-01 06:59:41 -0300

    Add missing `TestCase` import in `CollapseTest` and `OffcanvasTest` files. (#240)

--- tests/CollapseTest.php
+++ tests/CollapseTest.php
@@ -6,6 +6,7 @@ namespace Yiisoft\Yii\Bootstrap5\Tests;
 
 use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
 use Yiisoft\Yii\Bootstrap5\Collapse;
 use Yiisoft\Yii\Bootstrap5\Toggler;
 use Yiisoft\Yii\Bootstrap5\Tests\Support\Assert;
